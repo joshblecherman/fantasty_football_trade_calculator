7bf5ae7972adbfc592ebe7a3962aa36f20d73ec9
Josh Blecherman <jab1369@nyu.edu>
2025-09-17 17:06:11 -0400

    getting form on dropdown click

--- main.py
+++ main.py
@@ -4,15 +4,33 @@ from typing import Callable, List
 
 from nicegui import ui
 
-from typing import Type
+
+@dataclass
+class PendingAsset:
+    player = False
+    pick = False
+    faab = False
+    on_change: Callable
+
+    def pending_player(self):
+        self.player = True
+        self.on_change()
+
+    def pending_pick(self):
+        self.player = True
+        self.on_change()
+
+    def pending_faab(self):
+        self.player = True
+        self.on_change()
 
 
 @dataclass
 class Player:
-    name: str
-    projected_points: float
-    years_kept: int
-    games_remaining: int
+    name: str = None
+    projected_points: float = None
+    years_kept: int = None
+    games_remaining: int = None
 
 
 @dataclass
@@ -28,20 +46,20 @@ class FAAB:
     pass
 
 
-assetType = Type[Pick] | Type[Player] | Type[FAAB]
+Asset = Pick | Player | FAAB
 
 
 @dataclass
 class TradeAssets:
     title: str
     on_change: Callable
-    items: List[assetType] = field(default_factory=list)
+    items: List[Asset] = field(default_factory=list)
 
-    def add(self, asset: assetType) -> None:
+    def add(self, asset: Asset) -> None:
         self.items.append(asset)
         self.on_change()
 
-    def remove(self, asset: assetType) -> None:
+    def remove(self, asset: Asset) -> None:
         self.items.remove(asset)
         self.on_change()
 
@@ -50,17 +68,22 @@ class TradeAssets:
 def enter_trade_ui():
     ui.label('Enter a trade asset.').classes('mx-auto')
     # TODO, generate a form for each of the below 'assets'
-    # with ui.dropdown_button(text='select asset type', auto_close=True):
-    #     ui.item(text='player', on_click=lambda: trade_assets.add(asset_type=Player))
-    #     ui.item(text='pick', on_click=lambda: trade_assets.add(asset_type=Pick))
-    #     ui.item(text='player', on_click=lambda: trade_assets.add(asset_type=FAAB))
-    for item in trade_assets.items:
-        with ui.row().classes('items-center'):
-            ui.input(value=str(item.type)).classes('flex-grow').bind_value(item, 'name')
-            ui.button(on_click=lambda item=item: trade_assets.remove(item), icon='delete') \
-                .props('flat fab-mini color=grey')
+    with ui.dropdown_button(text='select asset type', auto_close=True):
+        ui.item(text='player', on_click=pending_asset.pending_player)
+
+    with ui.row().classes('items-center').bind_visibility(pending_asset, 'player'):
+        ui.input(value=str("Enter a player")).classes('flex-grow')
+        ui.button(icon='delete').props('flat fab-mini color=grey')
+        # ui.item(text='pick', on_click=lambda: trade_assets.add(asset_type=Pick))
+        # ui.item(text='player', on_click=lambda: trade_assets.add(asset_type=FAAB))
+    # for item in trade_assets.items:
+    #     with ui.row().classes('items-center'):
+    #         ui.input(value=str("Enter a player")).classes('flex-grow').bind_value(item, 'name')
+    #         ui.button(on_click=lambda item=item: trade_assets.remove(item), icon='delete') \
+    #             .props('flat fab-mini color=grey')
 
 
+pending_asset = PendingAsset(on_change=enter_trade_ui.refresh)
 trade_assets = TradeAssets('Sending', on_change=enter_trade_ui.refresh)
 
 with ui.card().classes('w-80 items-stretch'):

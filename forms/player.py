a48a609a1d3aa2c298c12c29d3ba2287a9cadaac
Josh Blecherman <jab1369@nyu.edu>
2025-09-19 00:10:03 -0400

    improved styling

--- forms/player.py
+++ forms/player.py
@@ -3,14 +3,23 @@ from nicegui import ui
 
 @ui.refreshable
 def player_form(asset_selection):
-    with ui.row().classes('items-center').bind_visibility(asset_selection, 'player') as row:
-        ui.button(icon='delete', on_click=asset_selection.cancel_player).props('fla fab-mini color=grey')
+    with ui.row().classes('flex-center').bind_visibility(asset_selection, 'player') as row:
+        with ui.column().classes('items-start gap-4'):  # main vertical layout
+            ui.button(icon='delete', on_click=asset_selection.cancel_player) \
+                .props('flat fab-mini color=grey')
 
-        ui.input(value=str("Enter a player")).classes('flex-grow') \
-            .bind_value_to(asset_selection.player_fields, 'projected_points')
-        ui.number(label='Projected Points', precision=1).props('clearable') \
-            .bind_value_to(asset_selection.player_fields, 'projected_points')
-        ui.radio([0, 1, 2, 3, 4], value=1).props('inline') \
-            .bind_value_to(asset_selection.player_fields, 'years_kept')
+            # Player name input
+            ui.input(value="Enter a player") \
+                .bind_value_to(asset_selection.player_fields, 'name')
+
+            # Projected points
+            ui.number(label='Projected Points', precision=1).props('clearable') \
+                .bind_value_to(asset_selection.player_fields, 'projected_points')
+
+            # Years kept
+            with ui.row().classes('items-center gap-2'):
+                ui.label(text='Years kept')
+                ui.radio([0, 1, 2, 3, 4]).props('inline') \
+                    .bind_value_to(asset_selection.player_fields, 'years_kept')
 
     return row
